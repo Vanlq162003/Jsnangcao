e0e1743d8b685b5b7fc2884c95496c29f83e170b
Vanlq162003 <113500569+Vanlq162003@users.noreply.github.com>
2023-01-14 09:52:22 +0700

    09-117

---
+++ 09-117. Maps Fundamentals/main.js
@@ -0,0 +1,45 @@
+const restaurant = {
+    name: 'Classico Italiano',
+    location1: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (starterIndex,mainIndex){
+        return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+    },
+    openingHours: {
+        thu: {
+          open: 12,
+          close: 22,
+        },
+        fri: {
+          open: 11,
+          close: 23,
+        },
+        sat: {
+          open: 0, // Open 24 hours
+          close: 24,
+        },
+      },
+    oderDelivery: function (obj){
+      console.log(obj);
+    },
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+      },
+};
+// Map: 
+const rest = new Map();
+console.log(rest);
+// set:truyền vào key và value
+rest.set('name', 'van');
+console.log(rest);
+
+
+// get: lấy value từ key
+console.log(rest.get('name'));
+console.log(rest.has('name1'));
+// clear: xóa tất cả trong Map
+rest.clear();
+console.log(rest);
