20d662813f43391759332d058d4e458838acaa6a
Vanlq162003 <113500569+Vanlq162003@users.noreply.github.com>
2023-01-16 11:03:47 +0700

    09-121

--- 09-121. Working With Strings - Part 1/main.js
+++ 09-121. Working With Strings - Part 1/main.js
@@ -29,3 +29,22 @@ const restaurant = {
         console.log(otherIngredients);
       },
 };
+
+// Lấy vị chữ trong chuỗi
+const name = 'Lê Quang Văn n';
+console.log(name[0]);
+console.log(name.indexOf('n')); // lấy chữ đầu
+console.log(name.lastIndexOf('n')); // lấy chữ cuối
+console.log(name.slice(4)); // lấy bắt đầu từ số 4 (nếu có tham só thứ 2 thì dừng lại ở tham số thứ 2)
+console.log(name.slice(4,5));
+
+console.log(name.slice(-2)); // số âm sẽ lấy từ cuối
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+  else console.log('You got lucky 😎');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
